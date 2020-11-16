18a365eb65eb3b3b1fe308e0ffb7bae568b369de
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2020-11-16 09:42:22 -0300

    Added jQuery

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function collapse(curso) {
     let simbol = document.querySelector(`span#${curso}`);
-    //window.alert(simbol);
     if (simbol.textContent == '+') {
         simbol.innerHTML = '-';
     } else if (simbol.textContent == '-') {
@@ -8,7 +7,7 @@ function collapse(curso) {
     }
 }
 
-$('.topo').click(function(e){
+$('.volta-topo').click(function(e){
     e.preventDefault();
     $('html, body').animate({
         scrollTop: 0
