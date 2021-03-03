1b47b3f2992fc71246694a624fd99dcf5e32f005
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-03-03 16:49:57 -0300

    Light Mode (First Edition)

--- index.js
+++ index.js
@@ -1,25 +1,101 @@
-/* Em produção
+/* Em produção*/
 function changeMode() {
-    let img = document.querySelector('img#img-lampada').src;
+    event.preventDefault();
+    const mode = localStorage.getItem('dark');
+    if (mode === 'true') {
+        changePhoto(1);
+        localStorage.setItem('dark', false);
+        acender();
+    } else if (mode === 'false') {
+        changePhoto(2);
+        localStorage.setItem('dark', true);
+        apagar();
+    }
+}
+
+function acender(){
+    const corpo = document.body;
+    const tith3 = document.querySelector('.masthead-brand');
+    const navLink = document.querySelectorAll('.nav-link');
+    const txtMuted = document.querySelector('.text-muted');
+    const links = document.querySelectorAll('[data-link="external"]');
+    const divContato = document.querySelectorAll('[data-div="contato"]');
+
+    corpo.classList.add('light');
+    tith3.classList.add('masthead-light');
+    navLink.forEach((item) => {
+        item.classList.add('nav-light');
+    });
+    txtMuted.classList.add('light');
+    links.forEach((item) => {
+        item.classList.add('light');
+    });
+    divContato.forEach((item) => {
+        item.classList.remove('border-dark');
+    });
+}
+
+function apagar(){
+    const corpo = document.body;
+    const tith3 = document.querySelector('.masthead-brand');
+    const navLink = document.querySelectorAll('.nav-link');
+    const txtMuted = document.querySelector('.text-muted');
+    const links = document.querySelectorAll('[data-link="external"]');
+    const divContato = document.querySelectorAll('[data-div="contato"]');
     
-    console.log(img);
-    *
-    if (img == apagada){
-        img.src = "img/lamp32px-acesa.png";
-    } else if (img == acesa){
-        img.src = "img/lamp32px-apagada.png";
-    }*
-}*/
+    corpo.classList.remove('light');
+    tith3.classList.remove('masthead-light');
+    navLink.forEach((item) => {
+        item.classList.remove('nav-light');
+    });
+    txtMuted.classList.remove('light');
+    links.forEach((item) => {
+        item.classList.remove('light');
+    });
+    divContato.forEach((item) => {
+        item.classList.add('border-dark');
+    });
+}
+
+function changePhoto(option){
+    let img = document.querySelector('img#img-lampada');
+    const local = img.src.split('img/')[0] + 'img/';
+    let nome = img.src.split('img/')[1];
+
+    switch (option){
+        case 1:
+            nome = 'lamp32px-acesa.png';
+            img.src = local + nome;
+            break;
+        case 2:
+            nome = 'lamp32px-apagada.png';
+            img.src = local + nome;
+            break;
+    }
+}
+
 
 (function() {
     $('[data-ativo="true"]').click();
+
+    if (!localStorage.getItem('dark')){
+        localStorage.setItem('dark', true);
+    }
+    const mode = localStorage.getItem('dark');
+    if (mode === 'true') {
+        changePhoto(2);
+        apagar();
+    } else {
+        changePhoto(1);
+        acender();
+    }
 })();
 
 function collapse(curso) {
     let simbol = document.querySelector(`span#${curso}`);
-    if (simbol.textContent == '+') {
+    if (simbol.textContent === '+') {
         simbol.innerHTML = '-';
-    } else if (simbol.textContent == '-') {
+    } else if (simbol.textContent === '-') {
         simbol.innerHTML = '+';
     }
 }
