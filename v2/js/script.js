ba2172d5b65716f0a21acdec873b0dd07a011d0c
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-04-26 14:38:08 -0300

    Slide for Portfolio

--- v2/js/script.js
+++ v2/js/script.js
@@ -1,6 +1,11 @@
 import {SlideNav} from './slide.js';
 
-const slide = new SlideNav('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide1', '.slide-wrapper1');
 slide.init();
-slide.addArrow('.prev', '.next');
-slide.addControl('.custom-controls');
+slide.addArrow('.prev1', '.next1');
+slide.addControl('.custom-controls1');
+
+const slide2 = new SlideNav('.slide2', '.slide-wrapper2');
+slide2.init();
+slide2.addArrow('.prev2', '.next2');
+slide2.addControl('.custom-controls2');
